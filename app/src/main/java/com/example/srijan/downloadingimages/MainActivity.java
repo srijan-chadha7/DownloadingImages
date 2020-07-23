a6c73833486d885a69495a2f254f1f575b8ecdca
srijan-chadha7 <srijan.chadha14@gmail.com>
2020-07-23 17:24:27 +0530

    Initial commit

---
+++ app/src/main/java/com/example/srijan/downloadingimages/MainActivity.java
@@ -0,0 +1,61 @@
+package com.example.srijan.downloadingimages;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.ExecutionException;
+
+public class MainActivity extends AppCompatActivity {
+    ImageView imageView;
+    public void downloadImage(View view){
+        DownloadImage task=new DownloadImage();
+        Bitmap myImage;
+        try {
+            myImage=task.execute("https://upload.wikimedia.org/wikipedia/en/a/aa/Bart_Simpson_200px.png").get();
+            imageView.setImageBitmap(myImage);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imageView=findViewById(R.id.imageView);
+
+    }
+    public class DownloadImage extends AsyncTask<String,Void,Bitmap> {
+
+
+        @Override
+        protected Bitmap doInBackground(String... urls) {
+
+            try {
+                URL url = new URL(urls[0]);
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                connection.connect();
+                InputStream in = connection.getInputStream();
+                Bitmap myBitmap = BitmapFactory.decodeStream(in);
+                return myBitmap;
+            } catch(Exception e) {
+                e.printStackTrace();
+                return null;
+            }
+
+        }
+    }
+}
